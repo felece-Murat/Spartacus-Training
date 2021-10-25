de31b735875359c226b09be9e6f72f724b7417a4
Murat-Yilmaz <mrt.yilmaz743@gmail.com>
2021-10-25 22:19:42 +0300

    Product page updates.

--- src/app/custom-pdp/custom-pdp.module.ts
+++ src/app/custom-pdp/custom-pdp.module.ts
@@ -2,20 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomProductIntroComponent } from './custom-product-intro/custom-product-intro.component';
 import { CmsConfig, ConfigModule } from '@spartacus/core';
-import { StarRatingModule } from '@spartacus/storefront';
+import { IconModule, StarRatingModule } from '@spartacus/storefront';
+import { CustomProductSummaryComponent } from './custom-product-summary/custom-product-summary.component';
 
 @NgModule({
   declarations: [
     CustomProductIntroComponent,
-    CustomProductIntroComponent
+    CustomProductIntroComponent,
+    CustomProductSummaryComponent
   ],
   imports: [
     CommonModule,
     StarRatingModule,
+    IconModule,
     ConfigModule.withConfig({
       cmsComponents: {
         ProductIntroComponent:{
           component: CustomProductIntroComponent
+        },
+        ProductSummaryComponent: {
+          component: CustomProductSummaryComponent
         } 
       }
 
